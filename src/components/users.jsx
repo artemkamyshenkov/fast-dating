c843713ce088ba8a249b18685ccfd6628b180ee9
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-03-02 17:55:30 +0300

    feat: add selected prof

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pagination from './pagination';
 import User from './user';
 import paginate from '../utils/paginate';
 import PropTypes from 'prop-types';
-
+import GroupList from './groupList';
+import api from '../api/index';
 const Users = ({ users, ...rest }) => {
   const count = users.length;
-
   const pageSize = 4;
   const [currentPage, setCurrentPage] = useState(1);
+  const [professions, setProfession] = useState();
+  const [selectedProf, setSelectedProf] = useState();
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfession(data));
+  }, []);
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex);
   };
-
+  const handleProfessionSelect = (item) => {
+    setSelectedProf(item);
+  };
   const userCrop = paginate(users, currentPage, pageSize);
   return (
     <>
+      {professions && <GroupList items={professions} onItemSelect={handleProfessionSelect} selectedItem={selectedProf} />}
+
       {count > 0 && (
         <table className="table">
           <thead>
