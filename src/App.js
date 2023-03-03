bd297ada7563f6c9213cc499ba65c4a6fb5275b2
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-03-03 13:28:13 +0300

    feat: add sorting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import api from './api';
+import React from 'react';
 import Users from './components/users';
 
 function App() {
-  const [users, setUsers] = useState();
-  useEffect(() => {
-    api.users.fetchAll().then((data) => setUsers(data));
-  }, []);
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => user._id !== userId));
-  };
-  const handleToggleBookMark = (id) => {
-    setUsers(
-      users.map((user) => {
-        if (user._id === id) {
-          return { ...user, bookmark: !user.bookmark };
-        }
-        return user;
-      })
-    );
-  };
-  return <div>{users && <Users users={users} onDelete={handleDelete} onToggleBookMark={handleToggleBookMark} />}</div>;
+  return <Users />;
 }
 
 export default App;
--- src/components/searchStatus.jsx
+++ src/components/searchStatus.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 const SearchStatus = ({ length }) => {
-  const renderPhrase = (number) => {
-    if (number === 0) return 'Никто не будет с тобой тусить, шляпа:(';
-    else if (number === 1) return '1 человек тусанет с тобой сегодня';
-    else if (number > 1 && number <= 4) return `${number} человека тусанет с тобой сегодня`;
-    else {
-      return `${number} человек тусанут с тобой сегодня`;
-    }
-  };
-  return (
-    <h2>
-      {' '}
-      <span className={'badge p-2 ' + (length > 0 ? 'bg-primary' : 'bg-danger')}>{renderPhrase(length)}</span>
-    </h2>
-  );
+    const renderPhrase = (number) => {
+        const lastOne = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) {
+            return "человек тусанет";
+        }
+        if (lastOne === 1) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        return "человек тусанет";
+    };
+    return (
+        <h2>
+            <span
+                className={"badge " + (length > 0 ? "bg-primary" : "bg-danger")}
+            >
+                {length > 0
+                    ? `${length + " " + renderPhrase(length)}   с тобой сегодня`
+                    : "Никто с тобой не тусанет"}
+            </span>
+        </h2>
+    );
 };
 SearchStatus.propTypes = {
-  length: PropTypes.number,
+    length: PropTypes.number
 };
+
 export default SearchStatus;
