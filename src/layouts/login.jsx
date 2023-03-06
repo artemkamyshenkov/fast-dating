f612d89bdbd1315b72d850a5b35de85e6eecab33
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-03-06 21:16:14 +0300

    feat: add login form

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,6 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TextField from '../components/textField';
+import validator from '../utils/validator';
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const [data, setData] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({});
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
+  const validateConfig = {
+    email: {
+      isRequired: { message: 'Email is required' },
+      isEmail: { message: 'Email is not correct' },
+    },
+    password: { isRequired: { message: 'Password is required' } },
+  };
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validate = () => {
+    const errors = validator(data, validateConfig);
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSummit = (e) => {
+    e.preventDefault();
+    const isvalid = validate();
+    if (!isvalid) return;
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSummit}>
+      <TextField
+        label="email"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+        error={errors.email}
+      />
+      <TextField
+        label="password"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+        error={errors.password}
+      />
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default Login;
