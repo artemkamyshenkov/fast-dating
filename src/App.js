c1f9aaa2e022c14b084c6e4513e5ad7b75349e99
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-03-14 15:58:14 +0300

    feat: add qualities hook

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import Login from './layouts/login';
 import EditPage from './components/page/editPage';
 import { ToastContainer } from 'react-toastify';
 import { ProfessionProvider } from './hooks/useProfession';
+import QualitiesProvider from './hooks/useQualities';
+
 function App() {
   return (
     <>
       <NavBar />
-      <ProfessionProvider>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/users/:userId?/" element={<Users />} />
-          <Route path="/users/:userId/edit" element={<EditPage />} />
-          <Route path="/login/:type?" element={<Login />} />
+      <QualitiesProvider>
+        <ProfessionProvider>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/users/:userId?/" element={<Users />} />
+            <Route path="/users/:userId/edit" element={<EditPage />} />
+            <Route path="/login/:type?" element={<Login />} />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </ProfessionProvider>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </ProfessionProvider>
+      </QualitiesProvider>
       <ToastContainer />
     </>
   );
