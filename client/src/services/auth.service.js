c3851910ea4b304d74f0486ed0f8087703e4d0c0
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-04-24 17:52:12 +0300

    start dev backend

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -4,7 +4,7 @@ import localStorageService from './localStorage.service';
 const httpAuth = axios.create({
   baseURL: 'https://identitytoolkit.googleapis.com/v1/',
   params: {
-    key: process.env.REACT_APP_FIREBASE_KEY,
+    key: 'AIzaSyB0M9PAhicE4ZbgEuyx5tzIpXBLGVBDxhU',
   },
 });
 
---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const chalk = require('chalk');
+const initDatabase = require('./startUp/initDatabase');
+const routes = require('./routes');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', routes);
+const PORT = config.get('port') ?? 8080;
+
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('prod');
+// } else {
+//   console.log('dev');
+// }
+
+async function start() {
+  try {
+    mongoose.connection.once('open', () => {
+      initDatabase();
+    });
+    await mongoose.connect(config.get('mongoUri'));
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}`))
+    );
+  } catch (error) {
+    console.log(chalk.red(error.message));
+    process.exit(1);
+  }
+}
+start();
